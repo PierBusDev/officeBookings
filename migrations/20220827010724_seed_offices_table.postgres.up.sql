3d98faa58bef250505d40abfa950a67d50f6897e
Pierpaolo Busacchi <pierbus.dev@gmail.com>
2022-08-27 03:12:54 +0200

    added migrations for seeding important tableS

---
+++ migrations/20220827010724_seed_offices_table.postgres.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO offices (office_name,created_at,updated_at) VALUES
+	 ('Big Office','2022-08-22 00:00:00.000','2022-08-22 00:00:00.000'),
+	 ('Medium Office','2022-08-23 00:00:00.000','2022-08-23 00:00:00.000'),
+	 ('Shared Office','2022-08-23 00:00:00.000','2022-08-23 00:00:00.000');
---
+++ migrations/20220827011154_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO restrictions (restriction_name,created_at,updated_at) VALUES
+	 ('Reservation','2022-08-22 00:00:00.000','2022-08-31 00:00:00.000'),
+	 ('Owner Block','2022-08-29 00:00:00.000','2022-08-30 00:00:00.000');
